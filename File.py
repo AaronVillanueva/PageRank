c15a18da6c541306c20b93abdfaeb6283a0f2901
Aaron Tonatiuh Villanueva Guzmán <30881287+AaronVillanueva@users.noreply.github.com>
2019-03-14 14:43:57 -0500

    Update File.py

--- File.py
+++ File.py
@@ -1,8 +1,9 @@
 print("reee")
 print("Bienvenido al programa de PageRank v.0001")
 
-
+estado="activo"
 while(estado="activo")
-
-  
-  
+  print("1 para subir")
+  print("2 para escribir")
+  list=input("Ingresa input")
+ 
